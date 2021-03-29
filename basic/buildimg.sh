53d4fdd5768c89f5841cf2a6fa832937a8813827
Eduardo Ahumada <eahumada@gmail.com>
2021-03-29 20:13:15 -0300

    Adds basic

---
+++ basic/buildimg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm A8E.data
+cat ATARIBAS.ROM > A8E.data
+wc -c < A8E.data
+cat ATARIXL.ROM >> A8E.data
+wc -c < A8E.data
+
+# to extract an image from .data : 
+# dd if=A8E.data of=ATARIXL.ROM skip=213200 count=16384 bs=1
+# https://stackoverflow.com/questions/1423346/how-do-i-extract-a-single-chunk-of-bytes-from-within-a-file/40792605
